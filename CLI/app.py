a9187fc168dd16733434fd9fe774567faff82951
jeremykariuki <mirujeremy@gmail.com>
2024-06-13 10:55:14 +0300

    creating CLI

--- CLI/app.py
+++ CLI/app.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
+ f
 from models.customer import Customer
 from models.table import Table
 from models.reservation import Reservation
